e98b00f1282928d195257eb5c7ca85cb775e0505
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-08-09 22:55:25 +0300

    LS added

--- src/App.js
+++ src/App.js
@@ -8,29 +8,44 @@ import './App.css';
 export default class App extends Component{
 
   state = {
-    contacts:[
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts:[],
     filter:'',
   }
 
+  componentDidMount(){
+    // console.log('contacts componentDidMount')
+    const persistedContacts = localStorage.getItem('contacts')
+    // проверяем или LS не возвращает null
+    if(persistedContacts){
+      this.setState({contacts: JSON.parse(persistedContacts) })
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    // console.log("contacts componentDidUpdate")
+    //Чтобы не создать бесконечный цикл, перезаписываем только по условию
+    if (prevState.contacts !== this.state.contacts){
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+    
+  }
+
+  //добавляем новый контакт, имя и номер возьмем из state ContactForm, id генерим тут
   addContact = ({name,number}) =>{
     const contact = {
       id:uuidv4(),
       name: name,
       number:number
     }
-
+  //распыляем старый массив и добавляем к нему новый контакт
     this.setState(prevState => {
       return{contacts: [...prevState.contacts,contact]}
     })
   }
 
   removeContact = contactId => {
-    console.log(contactId)
+    // console.log(contactId)
     this.setState(prevState=>{
       return {contacts: prevState.contacts.filter(({id}) => id !== contactId)}
     })
@@ -43,7 +58,7 @@ export default class App extends Component{
 
   getVisibleContacts = () =>{
     const {contacts,filter} = this.state
-    console.log(filter)
+    // console.log(filter)
     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
     
   }
